6db5093099d9c3c0a709e5f9b4252c0eb2122f6a
Riodino Raihan <riodinoraihan@gmail.com>
2025-07-13 14:17:43 +0700

    update hero link href

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,22 +22,22 @@ const Hero: React.FC<ProfileData> = ({ name, title, phone, email, linkedin, pers
         <p className="text-xl md:text-2xl text-slate-600 dark:text-slate-300 mb-6">{title}</p>
         <p className="text-md md:text-lg max-w-2xl mx-auto text-slate-500 dark:text-slate-400 mb-8">{personalStatement}</p>
         <div className="flex flex-wrap justify-center items-center space-x-4 md:space-x-6">
-          {phone && (
+          {/* {phone && (
             <a href={`tel:${phone}`} className="flex items-center text-sky-600 dark:text-sky-400 hover:text-sky-700 dark:hover:text-sky-300 transition-colors">
               <FaPhone className="mr-2" /> {phone}
             </a>
-          )}
+          )} */}
           {email && (
-            <a href={`mailto:${email}`} className="flex items-center text-sky-600 dark:text-sky-400 hover:text-sky-700 dark:hover:text-sky-300 transition-colors">
+            <a href={`mailto:riodinoraihan@gmail.com`} className="flex items-center text-sky-600 dark:text-sky-400 hover:text-sky-700 dark:hover:text-sky-300 transition-colors">
               <FaEnvelope className="mr-2" /> {email}
             </a>
           )}
           {linkedin && (
-            <a href={`https://linkedin.com/in/${linkedin}`} target="_blank" rel="noopener noreferrer" className="flex items-center text-sky-600 dark:text-sky-400 hover:text-sky-700 dark:hover:text-sky-300 transition-colors">
+            <a href={`https://linkedin.com/in/riodino-raihan`} target="_blank" rel="noopener noreferrer" className="flex items-center text-sky-600 dark:text-sky-400 hover:text-sky-700 dark:hover:text-sky-300 transition-colors">
               <FaLinkedin className="mr-2" /> LinkedIn
             </a>
           )}
-           <a href={`https://github.com/${linkedin}`} target="_blank" rel="noopener noreferrer" className="flex items-center text-sky-600 dark:text-sky-400 hover:text-sky-700 dark:hover:text-sky-300 transition-colors">
+           <a href={`https://github.com/riodino14`} target="_blank" rel="noopener noreferrer" className="flex items-center text-sky-600 dark:text-sky-400 hover:text-sky-700 dark:hover:text-sky-300 transition-colors">
               <FaGithub className="mr-2" /> GitHub
             </a>
         </div>
